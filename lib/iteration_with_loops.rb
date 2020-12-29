f5aca047431cdf67a6a4bfd21365c2bfd3607adb
John Fuquene <jfuquene@email.wm.edu>
2020-12-29 00:29:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ while row_index < src.count do
   while element_index < src[row_index].count do
     
    if src[row_index][element_index].class == String
-    sum = sum + src[row_index][element_index] + " "
+    sum = sum + src[row_index][element_index]
 
     
     end 
